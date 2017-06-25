341db6462d324aa17c1fb4e45d2bbe529fc18a78
Hunter Loftis <hunter@hunterloftis.com>
2017-06-25 19:42:33 -0400

    better error handling

--- diff.js
+++ diff.js
@@ -13,15 +13,17 @@ module.exports = {
 
 async function diff (context, heroku) {
   try {
+    // check files first to ensure that the apps exist
+    const files = await diffFiles(heroku, context.args.app1, context.args.app2)
+    // then check everything else together
     const diffs = await a.object({
-      files: diffFiles(heroku, context.args.app1, context.args.app2),
       env: diffEnv(heroku, context.args.app1, context.args.app2),
       stack: diffStack(heroku, context.args.app1, context.args.app2),
       bp: diffBuildpacks(heroku, context.args.app1, context.args.app2),
       addons: diffAddons(heroku, context.args.app1, context.args.app2),
       features: diffFeatures(heroku, context.args.app1, context.args.app2)
     })
-    const list = [...diffs.files, ...diffs.env, ...diffs.stack, ...diffs.bp, ...diffs.addons, ...diffs.features]
+    const list = [...files, ...diffs.env, ...diffs.stack, ...diffs.bp, ...diffs.addons, ...diffs.features]
     cli.table(list, {
       columns: [
         {key: 'prop', label: 'property' },
@@ -31,10 +33,7 @@ async function diff (context, heroku) {
     })
   }
   catch (err) {
-    console.error('Unable to diff:')
-    for (let e of err) {
-      console.error(e.stack)
-    }
+    throw err instanceof a.ErrorList ? err[0] : err
   }
 }
 
@@ -44,13 +43,20 @@ async function diffFiles(heroku, app1, app2) {
 }
 
 async function checksum(heroku, app) {
-  const releases = await heroku.get(`/apps/${app}/releases`)
-  if (releases && releases[0] && releases[0].slug) {
-    const slugID = releases[0].slug.id
-    const slug = await heroku.get(`/apps/${app}/slugs/${slugID}`)
-    return slug ? slug.checksum : null
+  try {
+    const releases = await heroku.get(`/apps/${app}/releases`)
+    if (releases && releases[0] && releases[0].slug) {
+      const slugID = releases[0].slug.id
+      const slug = await heroku.get(`/apps/${app}/slugs/${slugID}`)
+      return slug ? slug.checksum : null
+    }
+    return null
   }
-  return null
+  catch (err) {
+    if (err.statusCode === 404) {
+      throw new Error(`App not found: ${app}`)
+    }
+  }
 }
 
 async function diffEnv(heroku, app1, app2) {
@@ -60,7 +66,7 @@ async function diffEnv(heroku, app1, app2) {
   ])
   const keys = new Set(Object.keys(vars1).concat(Object.keys(vars2)))
   return Array.from(keys).filter(k => vars1[k] !== vars2[k]).map(k => {
-    return { prop: `config: ${k}`, app1: vars1[k], app2: vars2[k] }
+    return { prop: `config (${k})`, app1: vars1[k], app2: vars2[k] }
   })
 }
 
@@ -93,8 +99,10 @@ async function diffAddons(heroku, app1, app2) {
   ])
   const names1 = new Set(addons1.map(addon => addon.addon_service.name))
   const names2 = new Set(addons2.map(addon => addon.addon_service.name))
-  const only1 = [...names1].filter(name => !names2.has(name)).map(name => ({ prop: `add-on (${name})`, app1: 'true', app2: 'false' }))
-  const only2 = [...names2].filter(name => !names1.has(name)).map(name => ({ prop: `add-on (${name})`, app1: 'false', app2: 'true' }))
+  const only1 = [...names1].filter(name => !names2.has(name))
+    .map(name => ({ prop: `add-on (${name})`, app1: 'true', app2: 'false' }))
+  const only2 = [...names2].filter(name => !names1.has(name))
+    .map(name => ({ prop: `add-on (${name})`, app1: 'false', app2: 'true' }))
   return only1.concat(only2)
 }
 
@@ -105,7 +113,9 @@ async function diffFeatures(heroku, app1, app2) {
   ])
   const names1 = new Set(addons[0].map(feature => feature.enabled ? feature.name : null).filter(Boolean))
   const names2 = new Set(addons[1].map(feature => feature.enabled ? feature.name : null).filter(Boolean))
-  const only1 = [...names1].filter(name => !names2.has(name)).map(name => ({ prop: `feature (${name})`, app1: 'enabled', app2: 'disabled' }))
-  const only2 = [...names2].filter(name => !names1.has(name)).map(name => ({ prop: `feature (${name})`, app1: 'disabled', app2: 'enabled' }))
+  const only1 = [...names1].filter(name => !names2.has(name))
+    .map(name => ({ prop: `feature (${name})`, app1: 'enabled', app2: 'disabled' }))
+  const only2 = [...names2].filter(name => !names1.has(name))
+    .map(name => ({ prop: `feature (${name})`, app1: 'disabled', app2: 'enabled' }))
   return only1.concat(only2)
 }
